f363ae7e483828a331b2ba29b2d8be828232ad31
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-19 21:31:21 +0800

    changing document title icon logo, setting up a usereducer state for adding items to cart or removing item to cart.

--- src/Auth/CartContext/CartProvider.jsx
+++ src/Auth/CartContext/CartProvider.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./CartContext";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemCartHandler = () => {};
-  const removeItemCartHandler = () => {};
+  const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
+
+  const addItemCartHandler = (item) => {
+    dispatchCart({ type: "ADD", item: item });
+  };
+
+  const removeItemCartHandler = (id) => {
+    dispatchCart({ type: "REMOVE", id: id });
+  };
 
   return (
     <CartContext.Provider
       value={{
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemCartHandler,
         removeItem: removeItemCartHandler,
       }}>

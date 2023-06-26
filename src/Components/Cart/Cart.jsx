18587f4c8e9999d74429f260dcfd2977c85c408f
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-26 16:03:01 +0800

    Remove or decreasing the amount of the item in the cart.

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -10,7 +10,9 @@ const Cart = (props) => {
   const cartContext = useContext(CartContext);
   const hasItems = cartContext.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartContext.removeItem(id);
+  };
 
   const cartItemAddHandler = (item) => {
     cartContext.addItem({ ...item, amount: 1 });

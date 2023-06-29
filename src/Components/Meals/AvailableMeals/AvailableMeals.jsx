4dd24ed89bde978257b6f799c9f15c508f60d91d
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-29 13:06:33 +0800

    adding up photos to everytime user clicked the meals, there will be a photo modal showing up, importing jpg from assets.

--- src/Components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -3,24 +3,31 @@ import Card from "../../../UI/Card/Card";
 import MealItem from "../MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
+import tacos from "../../../assets/taco-images/4pcs tacos.jpg";
+import beshtaco from "../../../assets/taco-images/besh taco.jpg";
+import TacoCarnitas from "../../../assets/taco-images/4tacos.jpg";
+
 const DUMMY_MEALS = [
   {
     id: "m1",
     name: "Original Besh Taco",
     description: "lorem lorem lorem lorem",
     price: 69,
+    imgSrc: tacos,
   },
   {
     id: "m2",
     name: "Budget Taco",
     description: "lorem 1 pc of original taco with bigger size",
     price: 35,
+    imgSrc: beshtaco,
   },
   {
     id: "m3",
     name: "Taco Carnitas",
     description: "lorem lorem lorem lorem",
     price: 50,
+    imgSrc: TacoCarnitas,
   },
 ];
 
@@ -32,6 +39,7 @@ const AvailableMeals = () => {
       name={meal.name}
       description={meal.description}
       price={meal.price}
+      imgSrc={meal.imgSrc}
     />
   ));
 
--- src/Components/Meals/MealItem/MealItem.jsx
+++ src/Components/Meals/MealItem/MealItem.jsx
@@ -1,10 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import MealItemForm from "../MealItem/MealItemForm";
 import classes from "./MealItem.module.css";
 import CartContext from "../../../Auth/CartContext/CartContext";
+import logo from "../../../assets/taco-logo.jpg";
+
+import Modal from "../../../UI/Modal/Modal";
 
 const MealItem = (props) => {
   const cartContext = useContext(CartContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const addToCartHandler = (amount) => {
     cartContext.addItem({
@@ -15,15 +19,32 @@ const MealItem = (props) => {
     });
   };
 
+  const openModalHandler = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModalHandler = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     //FOR FUTURE REFERENCE, PUTTING UP A MODAL PHOTO OF EACH PRODUCTS EVERYTIME IT WILL BE CLICKED
 
     <li className={classes.meal}>
-      <div style={{ cursor: "pointer" }}>
+      <div style={{ cursor: "pointer" }} onClick={openModalHandler}>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>₱{props.price}</div>
       </div>
+      {isModalOpen && (
+        <Modal onClose={closeModalHandler}>
+          <img
+            src={props.imgSrc}
+            alt={props.name}
+            style={{ width: "100%", height: "500px" }}
+          />
+        </Modal>
+      )}
       <div>
         <MealItemForm onAddToCart={addToCartHandler} />
       </div>
